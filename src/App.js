f0b6bb6f23629dbf1d94a5bf1d60e4ff688c5385
Jean Furman <jean_11spfc@hotmail.com>
2023-07-14 19:10:40 -0300

    Adicionando filtro ao App principal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Filtro from './components/Filtro'
 
 function App() {
   return (
     <div className="App">
       <form className='formApp'>
         <div className='divForm'>
-          
+          <Filtro name="DataI" text="Data de Início" type="text"/>
+          <Filtro name="DataF" text="Data de Fim" type="text"/>
+          <Filtro name="NomeOperador" text="Nome do Operador" type="text"/>
+          <Filtro name="NumeroConta" text="Número da Conta" type="number"/>
         </div>
         <button className='btnPesquisa'>Pesquisar</button>
       </form>
