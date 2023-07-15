ae86eef6a6530f8b14ca1dde21abe9102ff6df4d
Jean Furman <jean_11spfc@hotmail.com>
2023-07-14 22:25:36 -0300

    Adicionando tabela no App principal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Filtro from './components/Filtro'
 import Saldo from './components/Saldo'
+import Tabela from './components/Tabela';
 
 function App() {
   return (
@@ -9,17 +10,14 @@ function App() {
         <div className='divForm'>
           <Filtro name="DataI" text="Data de Início" type="text"/>
           <Filtro name="DataF" text="Data de Fim" type="text"/>
-          <Filtro name="NomeOperador" text="Nome do Operador" type="text"/>
+          <Filtro name="NomeOperador" text="Nome do operado transacionado" type="text"/>
           <Filtro name="NumeroConta" text="Número da Conta" type="number"/>
         </div>
         <button className='btnPesquisa'>Pesquisar</button>
       </form>
       <div className='divApp'>
-        <Saldo />
-        <div>
-          
+          <Tabela/>
         </div>
-      </div>
     </div>
   );
 }
